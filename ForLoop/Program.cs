68309dbd9396faacba6c9bd5d6c84dc7703b8b33
Kevin <kevingm31@gmail.com>
2021-11-21 10:42:59 -0600

    Add ForLoop and SimpleMenu

---
+++ ForLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);  // Will print the numbers 0 to 4 in the terminal
+            }
+
+            //for (; ; )
+            //{
+                // Creates infinite loop by giving no sections
+            //}
+
+            for (int i = 9; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ SimpleMenu/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SimpleMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char userChoice = ' ';
+            while (!userChoice.Equals('q'))
+            {
+
+                Console.WriteLine("Please choose on of the following options");
+                Console.WriteLine("1. Cappucino");
+                Console.WriteLine("2. Latte");
+                Console.WriteLine("3. Americano");
+                Console.WriteLine("4. Mocha");
+                Console.WriteLine("5. Macchiato");
+                Console.WriteLine("6. Expresso");
+                Console.WriteLine("Q. Quit the program");
+
+                userChoice = Console.ReadKey(true).KeyChar;
+                Console.WriteLine("You chose " + userChoice);
+            }
+        }
+    }
+}
