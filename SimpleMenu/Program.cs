4b8b963171cf50f1e591dfe657669258a7db8a82
Kevin <kevingm31@gmail.com>
2021-11-24 12:12:52 -0600

    Add break and continue to SimpleMenu

--- SimpleMenu/Program.cs
+++ SimpleMenu/Program.cs
@@ -13,21 +13,43 @@ namespace SimpleMenu
             Console.WriteLine("3. Americano");
             Console.WriteLine("4. Mocha");
             Console.WriteLine("5. Macchiato");
-            Console.WriteLine("6. Expresso");
+            Console.WriteLine("6. Espresso");
             Console.WriteLine("Q. Quit the program");
             char userChoice = ' ';
-            //while (!userChoice.Equals('q'))
-            //{
-            //    Console.WriteLine("Please choose one of the menu options");
-            //    userChoice = char.ToLower(Console.ReadKey(true).KeyChar);
-            //    Console.WriteLine("You chose " + userChoice);
-            //}
-
+           
             do
             {
                 Console.WriteLine("Please choose one of the menu options");
                 userChoice = char.ToLower(Console.ReadKey(true).KeyChar);
                 Console.WriteLine("You chose " + userChoice);
+            
+
+
+                switch (userChoice)
+                {
+                    case '1':
+                        Console.WriteLine("Making the Cappucino");
+                        break;
+                    case '2':
+                        Console.WriteLine("Making the Latte");
+                        break;
+                    case '3':
+                        Console.WriteLine("Making the Americano");
+                        break;
+                    case '4':
+                        Console.WriteLine("Making the Mocha");
+                        break;
+                    case '5':
+                        Console.WriteLine("Making the Macchiato");
+                        break;
+                    case '6':
+                        Console.WriteLine("Making the Espresso");
+                        break;
+                    case 'q':
+                        Console.WriteLine("Quiting program");
+                        break;
+
+                }
             } while (!userChoice.Equals('q'));
         }
     }
